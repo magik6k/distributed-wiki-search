655eb2b2f68b224f5ac3c7ba9cf4e6dcbe04e3c6
Łukasz Magiera <magik6k@gmail.com>
2017-05-08 12:49:28 +0200

    Make putHashmap a little more readable

--- util/putHashmap.scala
+++ util/putHashmap.scala
@@ -4,56 +4,73 @@ import scala.io.Source
 import sys.process._
 import scala.language.postfixOps
 import java.io.ByteArrayInputStream
+import scala.collection.mutable
 
 object HahmapBucketsDouble extends App {
+  type Bucket = mutable.Map[String, String]
+  type L1 = mutable.Map[Int, Bucket]
+  type L0 = mutable.Map[Int, L1]
+
   val keyFile = args(0)
   val valueFile = args(1)
 
   val rawKeys = Source.fromFile(keyFile).getLines().toArray
   val rawValues = Source.fromFile(valueFile).getLines().toArray
-
   val data = rawKeys.zip(rawValues).toMap
 
   val hashedData = data.map(e => (e._1, (e._2, scala.util.hashing.MurmurHash3.stringHash(e._1))))
 
-  val root = scala.collection.mutable.Map[Int, Object]()
+  val root: L0 = mutable.Map[Int, L1]()
 
-  def getBucket[T, B1, B2](map: scala.collection.mutable.Map[Int, Object], bucket: Int): T = {
+  def getBucket[T <: mutable.Map[B1, B2], B1, B2, K](map: mutable.Map[K, T], bucket: K): T = {
     if(!map.contains(bucket))
-      map(bucket) = scala.collection.mutable.Map[B1, B2]()
-    map(bucket).asInstanceOf[T]
+      map(bucket) = mutable.Map[B1, B2]().asInstanceOf[T]
+    map(bucket)
   }
 
   hashedData.foreach {
     case (key, (value, hash)) =>
-      val l0 = ((hash & 0xfff00000) >>> 20)
-      val l1 = ((hash & 0x000ff000) >>> 12)
+      val l0 = (hash & 0xfff00000) >>> 20
+      val l1 = (hash & 0x000ff000) >>> 12
 
-      val lowBucket = getBucket[scala.collection.mutable.Map[Int, Object], Int, Object](root, l0)
-      val highBucket = getBucket[scala.collection.mutable.Map[String, String], String, String](lowBucket, l1)
+      val lowBucket = getBucket[L1, Int, Bucket, Int](root, l0)
+      val highBucket = getBucket[Bucket, String, String, Int](lowBucket, l1)
 
       highBucket(key) = value
   }
 
-  // Should have picked Selene/Go/C/Lua/Assembly/PHP for that....
-  var n = 0
+  var l0added = 0
 
   val rootObject = root.par.map {
-    case (l0: Int, l0bucket: Object) =>
-      val lowEntry = l0bucket.asInstanceOf[scala.collection.mutable.Map[Int, scala.collection.mutable.Map[String, String]]].map {
-        case (l1: Int, dataBucket: scala.collection.mutable.Map[String, String] @ unchecked) =>
-          val entry = "{\"data\":{" + (dataBucket.map{ case (k, v) => "\"" + esc(k) + "\": " + v }.mkString(",")) + "}}"
-          "\"" + l1.toHexString + "\":{\"/\":\"" + (Process(Array("./util/dagPut.sh", entry)).lineStream.head) + "\"}"
-      }.mkString(",")
+    case (l0: Int, l1bucket: L1) =>
 
-      if(n % 20 == 0)
-        Console.err.print(s"\r${Math.floor(n/40.92)}%    ")
-      n+=1
-      "\"" + l0.toHexString + "\":{\"/\":\"" + (Process(Array("./util/dagPut.sh", s"{$lowEntry}")).lineStream.head) + "\"}"     
+      if(l0added % 20 == 0)
+        Console.err.print(s"\r${Math.floor(l0added / 40.92)}%    ")
+      l0added += 1
+
+      "\"" + l0.toHexString + "\":{\"/\":\"" + addLowBucket(l1bucket) + "\"}"
   }.mkString(",")
 
   Console.err.println("\rdone          ")
-  println(("ipfs dag put"#< new ByteArrayInputStream(s"{$rootObject}".getBytes("UTF-8")) ).lineStream.head)
+  println(dagPut(s"{$rootObject}"))
 
-  def esc(str: String): String = str.replace("\\", "\\\\").replace("\"", "\\\"")
+  private def addLowBucket(bucket: L1): String = {
+    val lowEntry = bucket.map {
+      case (l1: Int, dataBucket: Bucket @ unchecked) =>
+        "\"" + l1.toHexString + "\":{\"/\":\"" + addHighBucket(dataBucket) + "\"}"
+    }.mkString(",")
+
+    Process(Array("./util/dagPut.sh", s"{$lowEntry}")).lineStream.head
+  }
+
+  private def addHighBucket(bucket: Bucket): String = {
+    val entry = "{\"data\":{" + bucket.map{ case (k, v) => "\"" + esc(k) + "\": " + v }.mkString(",") + "}}"
+    Process(Array("./util/dagPut.sh", entry)).lineStream.head
+  }
+
+  private def dagPut(in: String): String = {
+    ("ipfs dag put"#< new ByteArrayInputStream(in.getBytes("UTF-8")) ).lineStream.head
+  }
+
+  private def esc(str: String): String = str.replace("\\", "\\\\").replace("\"", "\\\"")
 }
